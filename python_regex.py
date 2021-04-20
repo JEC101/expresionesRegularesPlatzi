4b5f199cb36a82d06fcb8dc05f1415212da5a3bd
JEC101 <jecastro.agro@gmail.com>
2021-04-20 18:02:37 -0300

    python regex

--- python_regex.py
+++ python_regex.py
@@ -1,4 +1,13 @@
-#!////
-
 import re
 
+pattern = re.compile(r'^([\d]{4,4})\-\d\d\-\d\d,(.*),Friendly,.*$')
+
+f = open("./results_6aeb6252-c531-449d-bf29-e11193358b8c.csv", "r")
+
+for line in f:
+    res = re.match(pattern, line)
+    if res:
+        print("%s\n" % res.group(2))
+    
+
+f.close()
