5fb2910636c240bdc924efb72734fd56f16098f8
JEC101 <jecastro.agro@gmail.com>
2021-04-20 18:24:03 -0300

    python regex 2

--- python_regex.py
+++ python_regex.py
@@ -1,13 +1,15 @@
 import re
 
-pattern = re.compile(r'^([\d]{4,4})\-\d\d\-\d\d,(.*),Friendly,.*$')
+pattern = re.compile(r'^([\d]{4,4})\-\d\d\-\d\d,(.+),(.+),(\d+),(\d+),.*$')
 
 f = open("./results_6aeb6252-c531-449d-bf29-e11193358b8c.csv", "r")
 
 for line in f:
     res = re.match(pattern, line)
     if res:
-        print("%s\n" % res.group(2))
+        total = int(res.group(4)) + int(res.group(5))
+        if total > 10:
+            print(f"goles: {total}, en {res.group(1)}, entre {res.group(2)} {res.group(4)}-{res.group(5)} {res.group(3)}")
     
 
 f.close()
